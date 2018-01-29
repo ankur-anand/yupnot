54901785b0a112b12d050cb37d87eedde876dd93
Ankur Anand <ankur.anand12@gmail.com>
2018-01-29 13:26:01 +0530

    added goggle-passport-setup and nodemon

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
+const keys = require('./config/keys');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    accessToken => {
+      console.log(accessToken);
+    },
+  ),
+);
 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 app.listen(PORT);
