ada7f4c0f8e20c20f7b55dcd58883a2620ca7760
Ankur Anand <ankur.anand12@gmail.com>
2018-01-29 11:48:19 +0530

    configured app for heroku

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+app.listen(PORT);
