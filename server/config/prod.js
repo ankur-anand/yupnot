7c54a608c306c9105635811cbf53d671c2978512
Ankur Anand <ankur.anand12@gmail.com>
2018-01-31 15:38:54 +0530

    seperated dev and prod keys file

---
+++ server/config/prod.js
@@ -0,0 +1,7 @@
+// dont comit
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLEINT_SECRET,
+  mongoDbURI: process.env.MONGO_DB_URI,
+  cookieKeys: process.env.COOKIE_KEYS,
+};
