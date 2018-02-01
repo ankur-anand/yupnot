7979a209c01c6f01da29787f9e12f7c60d07105e
Ankur Anand <ankur.anand12@gmail.com>
2018-02-01 11:15:17 +0530

    initial react setup code

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render();
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
