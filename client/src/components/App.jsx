256b1c20999a1f99e7b59cd4e7fd319ec998b5f0
Ankur Anand <ankur.anand12@gmail.com>
2018-02-01 12:33:11 +0530

    configured react-router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-const App = () => <div>Hi There!</div>;
+const Header = () => <h2> Header </h2>;
+const DashBoard = () => <h2> DashBoard </h2>;
+const SurveyNew = () => <h2> SurveyNew </h2>;
+const Landing = () => <h2> landing </h2>;
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path="/" exact component={Landing} />
+        <Route path="/surveys" exact component={DashBoard} />
+        <Route path="/surveys/new" exact component={SurveyNew} />
+      </div>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,4 @@
+// Records Whether or not user is logged in
 export default function(state = {}, action) {
   switch (action.type) {
     default:
